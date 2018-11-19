bd0b47afd73bd11c1a20872ee67bc1c282a608e1
sergey-kochetov <git.kochetov@gmail.com>
2018-11-19 14:23:35 +0300

    java_curse_sb first commit

---
+++ src/main/java/edu/javacourse/studentorder/dao/DictionaryDao.java
@@ -0,0 +1,15 @@
+package edu.javacourse.studentorder.dao;
+
+import edu.javacourse.studentorder.domain.PassportOffice;
+import edu.javacourse.studentorder.domain.RegisterOffice;
+import edu.javacourse.studentorder.domain.Street;
+import edu.javacourse.studentorder.exception.DaoException;
+
+import java.util.List;
+
+public interface DictionaryDao
+{
+    List<Street> findStreets(String pattern) throws DaoException;
+    List<PassportOffice> findPassportOffices(String areaId) throws DaoException;
+    List<RegisterOffice> findRegisterOffices(String areaId) throws DaoException;
+}
